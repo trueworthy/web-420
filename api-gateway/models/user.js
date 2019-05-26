7b97e0aedfc4c0399c85d3ab1a82542e6431452b
Lea <ltrueworthy@yahoo.com>
2019-05-26 13:42:36 -0700

    api part 3

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -14,7 +14,17 @@ var userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
-
 /**
- Database queries
- */
+Database queries
+*/
+
+// user.save is used to add a new user in our database
+module.exports.add = (user, callback) => {
+    user.save(callback);
+}
+
+
+module.exports.getById = (id, callback) => {
+    var query = { _id: id };
+    User.findById(query, callback);
+}
