e40d30d614b70723e64810d7e948e771c4e280c5
Lea <ltrueworthy@yahoo.com>
2019-06-09 18:07:20 -0700

    update

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -1,33 +1,37 @@
-
 /**
  Fields username, password, and email
  */
 
-//creating userSchema
 var mongoose = require('mongoose');
 
-//userSchema incudes  username,password and email
+
 var userSchema = new mongoose.Schema({
     username: String,
     password: String,
     email: String
 });
 
+
 const User = module.exports = mongoose.model('User', userSchema);
 
-// user.save is used to add a new user in our database
+
+/**
+ Database queries
+ */
+
+// add a new user in our database
 module.exports.add = (user, callback) => {
     user.save(callback);
 };
 
-//exporting userSchema
-module.exports = mongoose.model('User', userSchema);
 
-//adds a new user to the database 
-module.exports.add = (user, callback) => {
-    user.save(callback)
-}
 module.exports.getById = (id, callback) => {
     var query = { _id: id };
     User.findById(query, callback);
-}
+};
+
+
+module.exports.getOne = (e, callback) => {
+    var query = { email: e };
+    User.findOne(query, callback);
+};
