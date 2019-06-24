9f7a0a4b7e371cfe303c45295bbe910aa85d9f52
Lea <ltrueworthy@yahoo.com>
2019-06-23 18:17:58 -0700

    token

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -29,26 +29,16 @@ exports.user_register = function (req, res) {
 
 // Verify token on GET
 exports.user_token = function (req, res) {
+    User.getById(req.userId, function (err, user) {
+        if (err) return res.status(500).send('There was a problem finding the user.');
 
-    var token = req.headers['x-access-token'];
+        if (!user) return res.status(404).send('No user found.');
 
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided' });
-
-    jwt.verify(token, config.web.secret, function (err, decoded) {
-        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-
-        User.getById(decoded.id, function (err, user) {
-            if (err) return res.status(500).send('There was a problem finding the user.');
-
-            if (!user) return res.status(404).send('No user found.');
-
-            res.status(200).send(user);
-        });
-
+        res.status(200).send(user);
     });
 };
 
-// Login as an existing user on POST
+// Login as an existing user in on POST
 exports.user_login = function (req, res) {
 
     User.getOne(req.body.email, function (err, user) {
--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -1,17 +1,9 @@
-/*============================================
-; Title: Assignment 2.3- api catalog
-; Author: Professor Krasso 
-; Date: 12 May 2019
-; Modified By: Lea Trueworthy
-; Description: API Routes
-;===========================================
-*/
-
 /**
  * API Routes
  */
 
 var express = require('express');
+var checkToken = require('../check-token');
 var router = express.Router();
 
 
@@ -23,7 +15,7 @@ router.post('/auth/register', auth_controller.user_register);
 
 
 // GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 
 
 // POST request for signing users in
